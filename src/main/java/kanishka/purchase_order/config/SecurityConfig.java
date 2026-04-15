77018e851f4818860c280853c0f9abf7541813c5
sriram <tallysriram007@gmail.com>
2026-04-15 12:24:57 +0000

    now backup before invoice step

--- src/main/java/kanishka/purchase_order/config/SecurityConfig.java
+++ src/main/java/kanishka/purchase_order/config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000", "http://18.61.211.13:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000", "http://18.61.253.234:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
